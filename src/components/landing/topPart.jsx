3406bbf271909e186d46d09e4c2f85a48c3773eb
Ian Kamau <kian99564@gmail.com>
2023-09-10 12:58:32 +0300

    Added images and did topPart.jsx

--- src/components/landing/topPart.jsx
+++ src/components/landing/topPart.jsx
@@ -32,11 +32,11 @@ export default function TopPart() {
       </div>
 
       <div className="topPartBottom">
-        <img src={Polygon2} alt="" width="300px" />
+        <img src={Polygon2} alt="" width="400px" />
 
         <div>
-          <img src={Polygon3} alt="" width="150px" />
-          <img src={Polygon4} alt="" width="150px" />
+          <img src={Polygon3} alt="" width="200px" />
+          <img src={Polygon4} alt="" width="200px" />
         </div>
       </div>
     </div>
