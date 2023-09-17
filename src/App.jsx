d5c53268d54af286443795e1c509f70736071d02
Ian Kamau <kian99564@gmail.com>
2023-09-17 11:35:05 +0300

    Adding about us page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Landing from "./Landing";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Items from "./components/shop/Items";
 import "./App.css";
+import AboutUs from "./pages/AboutUs";
 
 export default function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" exact="true" element={<Landing />} />
+        <Route path="/about-us" element={<AboutUs />} />
         <Route path="/shop" exact="true" element={<Items />} />
       </Routes>
     </BrowserRouter>
