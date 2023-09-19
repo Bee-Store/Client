2596776e867fccb8acd575d0b56f91da54492a65
Ian Kamau <kian99564@gmail.com>
2023-09-19 19:48:26 +0300

    Starting about page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/navbar/navbar";
+import HealthBenefit from "../components/about/healthBenefit/HealthBenefit";
 
 export default function AboutUs() {
   return (
     <div>
       <Navbar />
+      <HealthBenefit />
     </div>
   );
 }
