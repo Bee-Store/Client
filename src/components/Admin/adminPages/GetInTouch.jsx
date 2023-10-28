ed74b7e074125d47e55b134d19ee8f8333346a8c
Ian Kamau <kian99564@gmail.com>
2023-10-28 15:29:28 +0300

    Admin can now delete contact messages

--- src/components/Admin/adminPages/GetInTouch.jsx
+++ src/components/Admin/adminPages/GetInTouch.jsx
@@ -13,6 +13,7 @@ export default function GetInTouch() {
     <div className="mt-7 ml-12">
       {messages &&
         messages.map((message, index) => {
+          console.log(message._id)
           return (
             <div key={index}>
               <Group>
@@ -32,6 +33,18 @@ export default function GetInTouch() {
               <Text pl={54} pt="sm" size="sm">
                 {message.message}
               </Text>
+              <button
+                className="px-3 py-2 bg-red-600 text-black"
+                onClick={() => {
+                  fetch(`${import.meta.env.VITE_BASE_URL}contact/${message._id}`,{
+                    method: "DELETE",
+                  }).then((res) => res.json()).then((data) =>{
+                    console.log(data)
+                  });
+                }}
+              >
+                Delete
+              </button>
             </div>
           );
         })}
