4a7f49ee194971bf533fa7d7e22c9f01ad441362
Ian Kamau <kian99564@gmail.com>
2023-11-16 17:17:25 +0300

    Error handling OrderPage: User Dash

--- src/pages/dashboard/user/pages/Orders.jsx
+++ src/pages/dashboard/user/pages/Orders.jsx
@@ -1,7 +1,10 @@
+import { notifications } from "@mantine/notifications";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 export default function Orders() {
+  const navigate = useNavigate();
   const [products, setProducts] = useState([]);
   const userState = useSelector((state) => state.user);
   useEffect(() => {
@@ -13,19 +16,32 @@ export default function Orders() {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        if (data === "Token is not valid") {
+          notifications.show({
+            title: data,
+            message: "Token has expired, login to proceed.",
+          });
+          navigate("/");
+          window.location.reload();
+        }
         setProducts(data);
       });
   }, []);
 
-  products && console.log(products);
   return (
     <div className="flex flex-col gap-[4em] items-start justify-center mt-[3em]">
       {products &&
         products.map((product, index) => {
-          console.log(product.product);
           return (
-            <div key={index} className="flex justify-center items-center gap-[15px]">
-              <img src={product.product.image} alt="" className="w-[200px] rounded-md"/>
+            <div
+              key={index}
+              className="flex justify-center items-center gap-[15px]"
+            >
+              <img
+                src={product.product.image}
+                alt=""
+                className="w-[200px] rounded-md"
+              />
               <span>{product.product.name}</span>
               <span>Ksh {product.product.price}</span>
             </div>
