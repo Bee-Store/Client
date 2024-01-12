18648e01974f5c0dbd28636b7b7ff79633033619
Ian Kamau <kian99564@gmail.com>
2024-01-12 22:15:35 +0300

    changes

--- gitPush.sh
+++ gitPush.sh
@@ -4,6 +4,6 @@ git add .
 echo -e "\n Enter your commit message"
 read commit_message
 
-git commit -am commit_message
+git commit -am $commit_message
 
 git push
--- src/components/Admin/AdminProd.jsx
+++ src/components/Admin/AdminProd.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchProducts } from "../../features/product/productSlice";
-import Navbar from "../navbar/navbar";
+
 import {
   addToCart,
   removeFromCart,
