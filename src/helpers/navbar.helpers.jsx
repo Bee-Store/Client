2c13e1309d81d655bd75cd296e707b334c603dfd
Ian Kamau <kian99564@gmail.com>
2024-01-12 13:19:00 +0300

    Making changes

--- src/helpers/navbar.helpers.jsx
+++ src/helpers/navbar.helpers.jsx
@@ -1,7 +1,6 @@
 export const navLinks = [
   { tag: "Home", href: "/" },
   { tag: "Shop", href: "/shop" },
-  { tag: "cart", href: "/cart" },
   { tag: "About Us", href: "/about-us" },
   { tag: "Contact Us", href: "/contact-us" },
   { tag: "Team", href: "/team" },
