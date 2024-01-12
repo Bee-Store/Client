5ee2cc999d69b489ed8612f2c60e5298ce6c0ee3
Ian Kamau <kian99564@gmail.com>
2024-01-12 13:37:13 +0300

    ci cd

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./../dist/output.css"
+import "./output.css"
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './Store/store'
